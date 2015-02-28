27b190e1f52052f224c7661f1960fb0cec103271
Chen Sihang <seanchain2.0@gmail.com>
2015-02-28 23:47:42 +0800

    生成一个segue用于进行用户初次注册的喜好选择

---
+++ NewsReader/NewsReader/TagList.h
@@ -0,0 +1,13 @@
+//
+//  TagList.h
+//  NewsReader
+//
+//  Created by Sean Chain on 2/28/15.
+//  Copyright (c) 2015 Sean Chain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TagList : UIViewController
+
+@end
