57693da7d4befb2bca76838936ca2a57386ca150
Chen Sihang <seanchain2.0@gmail.com>
2015-03-01 11:24:55 +0800

    添加了用户自主选择的界面，实现初步的选择及高亮功能

--- NewsReader/NewsReader/Classes/DWTagList.m
+++ NewsReader/NewsReader/Classes/DWTagList.m
@@ -9,20 +9,20 @@
 #import <QuartzCore/QuartzCore.h>
 
 #define CORNER_RADIUS 5.0f
-#define LABEL_MARGIN 5.0f
-#define BOTTOM_MARGIN 5.0f
-#define FONT_SIZE 13.0f
-#define HORIZONTAL_PADDING 7.0f
-#define VERTICAL_PADDING 3.0f
-#define BACKGROUND_COLOR [UIColor blueColor]
-#define TEXT_COLOR [UIColor whiteColor]
+#define LABEL_MARGIN 15.0f
+#define BOTTOM_MARGIN 15.0f
+#define FONT_SIZE 16.0f
+#define HORIZONTAL_PADDING 12.0f
+#define VERTICAL_PADDING 9.0f
+#define BACKGROUND_COLOR [UIColor whiteColor]
+#define TEXT_COLOR [UIColor blackColor]
 #define TEXT_SHADOW_COLOR [UIColor whiteColor]
 #define TEXT_SHADOW_OFFSET CGSizeMake(0.0f, 1.0f)
-#define BORDER_COLOR [UIColor lightGrayColor].CGColor
+#define BORDER_COLOR [UIColor grayColor].CGColor
 #define BORDER_WIDTH 1.0f
 
 @implementation DWTagList
-
+NSMutableSet *set;
 @synthesize view, textArray;
 
 - (id)initWithFrame:(CGRect)frame
@@ -47,9 +47,43 @@
     [self display];
 }
 
+-(void)onClickUILable:(UITapGestureRecognizer *)sender{
+    
+    UITapGestureRecognizer *tap = (UITapGestureRecognizer*)sender;
+    
+    UILabel *la=(UILabel*)tap.view;
+    NSLog(@"%@", la.text);
+    CGSize textSize = [la.text sizeWithFont:[UIFont systemFontOfSize:FONT_SIZE] constrainedToSize:CGSizeMake(self.frame.size.width, 1500) lineBreakMode:UILineBreakModeWordWrap];
+    float width = textSize.width + 2 * HORIZONTAL_PADDING;
+    float height = textSize.height + 2 * VERTICAL_PADDING;
+    CGRect frame = CGRectMake((width - 5) * 0.88f, (height - 5) * 0.83f, 10, 10);
+    UIImageView *imgview = [[UIImageView alloc] initWithFrame:frame];
+    UIImage *img = [UIImage imageNamed:@"check.png"];
+    imgview.image = img;
+    if (la.textColor == [UIColor blackColor]) {
+        la.textColor = [UIColor redColor];
+        la.layer.borderColor = [UIColor redColor].CGColor;
+        [set addObject:la.text];
+        [la addSubview:imgview];
+        NSLog(@"%@", set);
+    }
+    else{
+        la.textColor = [UIColor blackColor];
+        la.layer.borderColor = [UIColor grayColor].CGColor;
+        for (UIImageView *imgv in [la subviews]) {
+            [imgv removeFromSuperview];
+        }
+        [set removeObject:la.text];
+        NSLog(@"%@", set);
+    }
+    
+    //相应代码
+    
+}
+
 - (void)display
 {
-    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(highlightedTextColor)];
+    set = [[NSMutableSet alloc] init];
     for (UILabel *subview in [self subviews]) {
         [subview removeFromSuperview];
     }
@@ -61,7 +95,6 @@
         textSize.width += HORIZONTAL_PADDING*2;
         textSize.height += VERTICAL_PADDING*2;
         UILabel *label = nil;
-        [label addGestureRecognizer:tap];
         if (!gotPreviousFrame) {
             label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, textSize.width, textSize.height)];
             totalHeight = textSize.height;
@@ -87,12 +120,15 @@
         [label setTextColor:TEXT_COLOR];
         [label setText:text];
         [label setTextAlignment:UITextAlignmentCenter];
-        [label setShadowColor:TEXT_SHADOW_COLOR];
-        [label setShadowOffset:TEXT_SHADOW_OFFSET];
+//        [label setShadowColor:TEXT_SHADOW_COLOR];
+//        [label setShadowOffset:TEXT_SHADOW_OFFSET];
         [label.layer setMasksToBounds:YES];
         [label.layer setCornerRadius:CORNER_RADIUS];
         [label.layer setBorderColor:BORDER_COLOR];
         [label.layer setBorderWidth: BORDER_WIDTH];
+        UITapGestureRecognizer *tapgesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onClickUILable:)];
+        [label addGestureRecognizer:tapgesture];
+        label.userInteractionEnabled = YES;
         [self addSubview:label];
     }
     sizeFit = CGSizeMake(self.frame.size.width, totalHeight + 1.0f);
