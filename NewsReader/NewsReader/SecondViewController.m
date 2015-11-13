1c09ead247c467fed8df89d3ffc7252ab45fe927
Sean Chain <seanchain2.0@gmail.com>
2015-11-13 10:51:39 +0800

    fix something

--- NewsReader/NewsReader/SecondViewController.m
+++ NewsReader/NewsReader/SecondViewController.m
@@ -9,6 +9,7 @@
 #import "SecondViewController.h"
 #import "TableViewCell.h"
 #import "UUColor.h"
+#import "CustomTableViewCell.h"
 
 @interface SecondViewController ()
 
@@ -30,29 +31,60 @@
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 2;
+    return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return section?1:1;
+    NSInteger returnval = 0;
+    switch (section) {
+        case 0:
+            returnval = 1;
+            break;
+        case 1:
+            returnval = 1;
+            break;
+        case 2:
+            returnval = 8;
+            break;
+        default:
+            break;
+    }
+    return returnval;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *cellIdentifier = @"TableViewCell";
-    
-    TableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-    
-    if(cell == nil) {
-        cell = [[[NSBundle mainBundle]loadNibNamed:@"TableViewCell" owner:nil options:nil] firstObject];
+    if (indexPath.section == 0 || indexPath.section == 1) {
+        TableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+        
+        if(cell == nil) {
+            cell = [[[NSBundle mainBundle]loadNibNamed:@"TableViewCell" owner:nil options:nil] firstObject];
+        }
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        [cell configUI:indexPath];
+        return cell;
     }
-    cell.selectionStyle = UITableViewCellSelectionStyleNone;
-    [cell configUI:indexPath];
-    
-    return cell;
+    else {
+        static NSString* cellId = @"cellId";
+        CustomTableViewCell* cell = (CustomTableViewCell*)[tableView
+                                                           dequeueReusableCellWithIdentifier:cellId];
+        if(cell == nil) {
+            NSArray *nibArray = [[NSBundle mainBundle] loadNibNamed:@"CustomTableViewCell" owner:self options:nil];
+            cell = [nibArray objectAtIndex:0];
+        }
+        cell.titleLabel.text = @"测试";
+        cell.timeLabel.text = @"时间测试";
+        return cell;
+    }
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return 200;
+    if (indexPath.section == 0 || indexPath.section == 1)
+        return 200;
+    else {
+        UITableViewCell *cell = [self tableView:tableView cellForRowAtIndexPath:indexPath];
+        return cell.frame.size.height;
+    }
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
@@ -64,7 +96,9 @@
     UILabel *label = [[UILabel alloc]initWithFrame:frame];
     label.font = [UIFont systemFontOfSize:25];
     label.backgroundColor = UULightBlue;
-    label.text = section ? @"新闻分类排名":@"新闻日期走势";
+    if (section == 0) label.text = @"新闻分类排名";
+    else if (section == 1) label.text = @"新闻日期走势";
+    else label.text = @"推荐新闻";
     label.textColor = UUWhite;
     label.textAlignment = NSTextAlignmentCenter;
     return label;
